18ae4f0c37e7808ad6f5ea07e0e201c479b7e062
cbganer@gmail.com <caiobarbosa2404@hotmail.com>
2023-02-16 15:43:27 -0300

    Pattern Proxy

--- src/TesteComposicao.java
+++ src/TesteComposicao.java
@@ -2,6 +2,7 @@ import java.math.BigDecimal;
 
 import br.com.caio.loja.orcamento.ItemOrcamento;
 import br.com.caio.loja.orcamento.Orcamento;
+import br.com.caio.loja.orcamento.OrcamentoProxy;
 
 public class TesteComposicao {
 
@@ -15,7 +16,10 @@ public class TesteComposicao {
 		novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 		novo.adicionarItem(antigo);
 		
-		System.out.println(novo.getValor());
+		OrcamentoProxy proxy = new OrcamentoProxy(novo);
+		
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
 	}
 	
 }
